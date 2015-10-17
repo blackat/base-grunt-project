0602f62ae959fe5f59ab8f65157ced30046823be
blackat <eugenio_lentini@fastmail.fm>
2015-10-17 17:57:12 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// wrapper function
+module.exports = function (grunt) {
+
+    // Load grunt tasks automatically, when needed
+    require('jit-grunt')(grunt, {});
+
+    // Time how logn a task take
+    require('time-grunt');
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
+}
