3a68ca84b5a1cfabc4ca1e00611990afbc226e09
blackat <eugenio_lentini@fastmail.fm>
2015-10-18 06:19:12 +0200

    adde bower and wiredep

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,13 @@ module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+
+        wiredep: {
+            target: {
+                src: 'client/index.html'
+            }
+        }
     });
+
 }
