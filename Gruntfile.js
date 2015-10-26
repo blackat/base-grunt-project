d9f456520bff7379fd38ae3623a2dd4e6cbac46c
blackat <eugenio_lentini@fastmail.fm>
2015-10-26 09:04:59 +0100

    added karma coverage and reporters

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,16 +6,36 @@ module.exports = function (grunt) {
     // Load grunt tasks automatically, when needed
     require('jit-grunt')(grunt, {});
 
-    // Time how logn a task take
+    // Time how long a task take
     require('time-grunt');
 
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        // inject bower dependencies
         wiredep: {
             target: {
                 src: 'client/app/index.html'
+            }
+        },
+
+        karma: {
+            options: {
+                configFile: 'karma.conf.js',
+                reporters: ['progress', 'coverage'],
+                logLevel: 'INFO',
+                coverageReporter: {
+                    type: 'html',
+                    dir: 'coverage/'
+                }
+            },
+            continuous: {
+                singleRun: true,
+                logLevel: 'DEBUG'
+            },
+            dev: {
+                singleRun: false
             }
         }
     });
--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,9 @@ module.exports = function (config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+        preprocessors: {
+            'client/app/**/!(*.spec).js': 'coverage'
+        },
 
 
         // test results reporter to use
@@ -69,5 +71,5 @@ module.exports = function (config) {
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
         singleRun: false
-    })
-}
+    });
+};
